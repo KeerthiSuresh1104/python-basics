af9706c9f584587d4fe628eeb9df8fb5b770bc94
KeerthiSuresh1104 <keerthisuresh1104@gmail.com>
2025-12-23 15:58:17 +0530

    Add basic programs for name, even/odd, and sum
    Fix syntax errors in even/odd check and sum calculation.

---
+++ day1.py
@@ -0,0 +1,14 @@
+#program 1 :print name
+print("my name is keerthi")
+
+#program 2: even or odd
+num=10
+if num%2==o:
+     print("even")
+else:
+      print("odd")
+
+#program 3: sum of 2 numbers
+a=10
+b=23
+print("sum=",a+b)
