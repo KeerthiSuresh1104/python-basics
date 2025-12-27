c97831bdf9d5b60c28cdf1ea0574664db13cd5bf
KeerthiSuresh1104 <keerthisuresh1104@gmail.com>
2025-12-27 12:20:22 +0530

    Add list programs

---
+++ day5.py
@@ -0,0 +1,18 @@
+#program 1:Print all elements
+
+nums = [2,4,6,7,8]
+for n in nums:
+  print(n)
+
+#program 2:Sum of list elements
+
+num=[2,5,6,7]
+total = 0
+for n in nums:
+  total += n
+print(total)
+
+#program 3: Find max value
+
+nums=[20,78,98]
+print(max(nums))
