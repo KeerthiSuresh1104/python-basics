761e4d9f73c307d6ed418b52ec491335b0ae7255
KeerthiSuresh1104 <keerthisuresh1104@gmail.com>
2025-12-28 21:02:42 +0530

    Correct syntax and logic in day6.py
    Fix syntax errors in loops and print statements.

---
+++ day6.py
@@ -0,0 +1,20 @@
+#1:print numbers from 1 to 10
+for i in range[1,11]:
+  print(i)
+
+#2:print each character in a string
+name = "keerthi"
+for ch in name:
+  print(ch)
+
+#3:print even numbers from the list
+num=[10,35,44,60,32]
+for n in num:
+  if n%2==0:
+    print(n)
+
+#4:sum of numbers from 1 to 5
+total = 0
+for i in range[1, 6]
+  total +=i
+  print ("sum:"total)
